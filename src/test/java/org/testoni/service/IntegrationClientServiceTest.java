e6165548d6d9062cd2c86029634ca6ec064a072f
gabriel testoni <gabriel.testoni@gmail.com>
2023-09-23 20:17:40 -0300

    configured build unit tests, remove junit deprecated

--- src/test/java/org/testoni/service/IntegrationClientServiceTest.java
+++ src/test/java/org/testoni/service/IntegrationClientServiceTest.java
@@ -1,13 +1,15 @@
 package org.testoni.service;
 
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.testoni.utils.FileParserBuilder;
 import org.testoni.utils.FileReaderBuilder;
 
 import java.io.IOException;
 import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IntegrationClientServiceTest {
     private IntegrationClientService integrationClientService = null;
@@ -18,8 +20,10 @@ public class IntegrationClientServiceTest {
 
     @Test
     public void testProcessLineValid() {
-        URL path1 = getClass().getResource("/data_1.txt");
+        //URL path1 = getClass().getResource("/");
+        URL path1 = getClass().getResource("/data_2.txt");
         String jsonUsersIntegration = new IntegrationClientService(new FileReaderBuilder(), new FileParserBuilder()).getJsonUsersIntegration(path1.getPath());
-        System.out.println(jsonUsersIntegration);
+        //System.out.println(jsonUsersIntegration);
+        assertTrue(true);
     }
 }
